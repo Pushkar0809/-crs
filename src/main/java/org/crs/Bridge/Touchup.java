e585c6719bc2379c9a3807e45ec766a71dccf5a7
pushkar <pushkar.kumar@staybloom.om>
2025-07-21 17:05:14 +0530

    code refactored

--- src/main/java/org/crs/Bridge/Touchup.java
+++ src/main/java/org/crs/Bridge/Touchup.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.time.Duration;
-
 public class Touchup {
     public static void main(String[] args) throws InterruptedException {
 
